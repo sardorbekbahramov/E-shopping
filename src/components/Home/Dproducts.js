8450f8760f3b2a9c2d089adee972c2e4eee6b50e
Sardorbek <sardorbekbahramov04@gmail.com>
2024-02-01 17:44:46 +0500

    created home page

---
+++ src/components/Home/Dproducts.js
@@ -0,0 +1,118 @@
+import img1 from "../../images/image/p1.webp";
+import img2 from "../../images/image/p2.webp";
+import img3 from "../../images/image/p3.webp";
+import img4 from "../../images/image/p4.webp";
+import img5 from "../../images/image/p5.webp";
+import img6 from "../../images/image/p6.webp";
+import img7 from "../../images/image/p7.webp";
+import img8 from "../../images/image/p8.webp";
+import img9 from "../../images/image/p9.jpg";
+import img10 from "../../images/image/p10.jpg";
+import img11 from "../../images/image/p11.jpg";
+
+
+
+
+
+
+
+const Homeproducts = [
+    {
+        id: 1,
+        name: 'XT 55 samsung',
+        price: "126",
+        img : img1,
+        cat: '',
+        type: 'new'
+    },
+    {
+        id: 2,
+        name: 'mc5 pro apple',
+        price: "66",
+        img : img2,
+        cat: '',
+        type: 'new'
+    },
+    {
+        id: 3,
+        name: 'smartwatch',
+        price: "84",
+        img : img3,
+        cat: '',
+        type: 'new'
+    },
+    {
+        id: 4,
+        name: 'audio mic200',
+        price: "657",
+        img : img4,
+        cat: '',
+        type: 'new'
+    },
+    {
+        id: 5,
+        name: 'cctv online',
+        price: "212",
+        img : img5,
+        cat: '',
+        type: 'featured'
+    },
+    {
+        id: 6,
+        name: 'xtech mi',
+        price: "34",
+        img : img6,
+        cat: '',
+        type: 'featured'
+    },
+
+    {
+        id: 7,
+        name: 'earpods',
+        price: "421",
+        img : img7,
+        cat: '',
+        type: 'featured'
+    },
+    {
+        id: 8,
+        name: 'audio mic new',
+        price: "32",
+        img : img8,
+        cat: '',
+        type: 'top'
+    },
+    {
+        id: 9,
+        name: 'smartwatch',
+        price: "314",
+        img : img3,
+        cat: '',
+        type: 'top'
+    },
+    {
+        id: 10,
+        name: 'ipad apple',
+        price: "354",
+        img : img9,
+        cat: '',
+        type: 'top'
+    },
+    {
+        id: 11,
+        name: 'headphone',
+        price: "260",
+        img : img10,
+        cat: '',
+        type: 'top'
+    },
+    {
+        id: 12,
+        name: 'refeere tv',
+        price: "126",
+        img : img11,
+        cat: '',
+        type: 'top'
+    },
+]
+export default Homeproducts;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TbPlayerTrackNext } from "react-icons/tb";
+import { FaEye, FaHeart, FaFacebookF, FaTwitter, FaInstagram, FaYoutube  } from "react-icons/fa";
+import gift from "../../images/image/download.png";
+import imgTest from "../../images/image/T1.avif";
 import "./home.css";
 import {Link} from "react-router-dom";
+import Homeproducts from './Dproducts';
 
 
 const Home = () => {
+    const [trendingProduct, setTrendingProduct] = useState(Homeproducts);
+
+//====== filter of home products ===================>
+    const filtercate = (type) => {
+        const filterproduct = Homeproducts.filter((currEl)=>{
+            return currEl.type === type;
+        })
+        setTrendingProduct(filterproduct);
+    }
+
+    // all trending products:
+    const allTrendingProducts = ()=> {
+        setTrendingProduct(Homeproducts);
+    }
+//====== filter of home products finished ===========>
+
     return (
         <>
             <div className="home">
@@ -23,20 +43,110 @@ const Home = () => {
                         <div className="left_box">
                             <div className="header">
                                 <div className="heading">
-                                    <h2>trending products</h2>
+                                    <img src={gift} alt="" />
+                                    <h2 onClick={allTrendingProducts} >trending products</h2>
                                 </div>
                                 <div className="cate">
-                                    <h3>New</h3>
-                                    <h3>Featured</h3>
-                                    <h3>Top Selling</h3>
+                                    <h3 onClick={()=>filtercate('new')}>New</ h3>
+                                    <h3 onClick={()=>filtercate('featured')}>Featured</ h3>
+                                    <h3 onClick={()=>filtercate('top')}>Top Selling</h3>
+                                </div>
+                            </div>
+
+                            <div className="products">
+                                <div className="container">
+                                    {
+                                        trendingProduct.map((item)=>{
+                                            return(
+                                                <>
+                                                    <div className="box">
+                                                        <div className="img_box">
+                                                            <img src={item.img} alt="productImg" />
+                                                            <div className="icon">
+                                                                <div className="icon_box">
+                                                                    <FaEye />
+                                                                </div>
+                                                                <div className="icon_box">
+                                                                    <FaHeart />
+                                                                </div>
+                                                            </div>
+                                                        </div>
+
+                                                        <div className="info">
+                                                            <h3>{item.name}</h3>
+                                                            <p>${item.price}</p>
+                                                            <button className='btn'>Add To Cart</button>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            )
+                                        })
+                                    }
+                                </div>
+
+                                <div className="show_more">
+                                    <button>Show More</button>
+                                </div>
+                            </div>
+                        </div>
+                        
+                         
+                        <div className="right_box">
+                            <div className="right_container">
+                                <div className="testimonial">
+                                    <div className="head">
+                                        <h3>Our testmonial</h3>
+                                    </div>
+
+                                    <div className="detail">
+                                        <div className="img_box">
+                                            <img src={imgTest} alt="testimonial_img" />
+                                        </div>
+                                        <div className="info">
+                                            <h3>stephan robot</h3>
+                                            <h4>web designer</h4>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptatem necessitatibus exercitationem doloremque libero quae.</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="newsletter">
+                                    <div className="head">
+                                        <h3>News Letter</h3>
+                                    </div>
+                                    <div className="form">
+                                        <p>join our malling list</p>
+                                        <form>
+                                            <input required type="email" placeholder='E-mail' autoComplete='off'/>
+                                            <div className="btn">
+                                                <button type='submit'>subscribe</button>
+                                            </div>
+                                        </form>
+                                        <div className="icon_box">
+                                            <div className="icon">
+                                                <FaFacebookF  className='icon_id'/>
+                                            </div>
+                                            <div className="icon">
+                                                <FaTwitter className='icon_id'/>
+                                            </div>
+                                            <div className="icon">
+                                                <FaInstagram className='icon_id'/>
+                                            </div>
+                                            <div className="icon">
+                                                <FaYoutube className='icon_id'/>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="right_box">
+                    </div>
+                </div>
 
-                        </div>
-
+                <div className="banners">
+                    <div className="container">
+                        
                     </div>
                 </div>
             </div>
